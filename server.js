06810d9397972b6109159f8fd485fd5d4a544157
Jibreel Campbell <jycampbell7@gmail.com>
2022-01-23 10:46:43 -0500

    added config.env file and installed multiple npm packages

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}....`);
-})
+  console.log(`server is running on port ${PORT}....`);
+});
