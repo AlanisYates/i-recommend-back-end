af93a3fd85f711701512c4730b3de8898ccbb3dc
Jibreel Campbell <jycampbell7@gmail.com>
2022-01-23 12:08:21 -0500

    Connected to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({ path: "./config.env" });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+let database = process.env.DATABASE;
+database = database.replace("<password>", process.env.DATABASE_PASSWORD);
+//console.log(database);
+
+mongoose.connect(database).then(() => {
+  console.log("Database connection successful");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
