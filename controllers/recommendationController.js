97e1a45e0cf9be6db368c636851dd79fc89b4eab
Jibreel Campbell <jycampbell7@gmail.com>
2022-02-03 12:23:23 -0500

    finished basic recommendations route

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -26,3 +26,94 @@ exports.createRec = async function (req, res, next) {
     next(err);
   }
 };
+
+exports.getRec = async function (req, res, next) {
+  try {
+    const id = req.params.id;
+    const rec = await RecModel.findById(id);
+
+    if (!rec) {
+      return next(new Error("Recommendation ID is invalid."));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        recommendation: rec,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getRecs = async function (req, res, next) {
+  try {
+    const recs = await RecModel.find();
+
+    res.status(200).json({
+      status: "success",
+      results: recs.length,
+      data: {
+        recommendations: recs,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateRec = async function (req, res, next) {
+  try {
+    const id = req.params.id;
+
+    const body = {
+      name: req.body.name,
+      generalLocation: req.body.generalLocation,
+      address: req.body.address,
+      images: req.body.images,
+      description: req.body.description,
+      cost: req.body.cost,
+      businessType: req.body.businessType,
+      website: req.body.website,
+      googleMapsLink: req.body.googleMapsLink,
+    };
+
+    const updatedRec = await RecModel.findByIdAndUpdate(id, body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedRec) {
+      return next(new Error("Recommendation ID is invalid."));
+    }
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        recommendation: updatedRec,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteRec = async function (req, res, next) {
+  try {
+    const id = req.params.id;
+
+    const rec = await RecModel.findByIdAndDelete(id);
+
+    if (!rec) {
+      return next(new Error("Recommendation ID is invalid."));
+    }
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/recRoutes.js
+++ routes/recRoutes.js
@@ -3,6 +3,11 @@ const recController = require("../controllers/recommendationController");
 
 const router = express.Router();
 
-router.route("/").post(recController.createRec);
+router.route("/").post(recController.createRec).get(recController.getRecs);
+router
+  .route("/:id")
+  .get(recController.getRec)
+  .patch(recController.updateRec)
+  .delete(recController.deleteRec);
 
 module.exports = router;
